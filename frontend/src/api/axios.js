ed730d314bace61848560fc7233f2f27ba10296a
efzetx0-wq <efzetmedusa@gmail.com>
2026-06-03 13:07:14 -0400

    fix

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: "health-monitoring-production.up.railway.app/api",
+  baseURL: "https://health-monitoring-production.up.railway.app/api",
 });
 
 api.interceptors.request.use((config) => {
