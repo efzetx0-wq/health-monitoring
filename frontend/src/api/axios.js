e9a846e669e7d890f3c5a24681036c0932d06962
efzetx0-wq <efzetmedusa@gmail.com>
2026-06-04 01:27:55 -0400

    update

--- frontend/src/api/axios.js
+++ frontend/src/api/axios.js
@@ -2,19 +2,17 @@ import axios from "axios";
 
 const api = axios.create({
   baseURL: "https://health-monitoring-production.up.railway.app/api",
+  withCredentials: true, // <--- WAJIB DITAMBAHKAN AGAR COOKIE SANCTUM BISA LEWAT LINTAS DOMAIN
 });
 
 api.interceptors.request.use((config) => {
-
   const token = localStorage.getItem("token");
 
   if (token) {
-    config.headers.Authorization =
-      `Bearer ${token}`;
+    config.headers.Authorization = `Bearer ${token}`;
   }
 
   return config;
 });
 
 export default api;
-
