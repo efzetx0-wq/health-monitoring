781118fa9e6d4d27ede89c973ac5d95a8e96515c
efzetx0-wq <efzetmedusa@gmail.com>
2026-06-04 04:09:28 -0400

    fix sintaks array cors dan siapkan procfile

--- backend/config/cors.php
+++ backend/config/cors.php
@@ -1,17 +1,21 @@
-'paths' => ['api/*', 'sanctum/csrf-cookie'],
+<?php
 
-'allowed_methods' => ['*'],
+return [
 
-'allowed_origins' => [
-    'https://health-monitoring-peach.vercel.app', // WAJIB masukkan domain Vercel Anda di sini
-],
+    'paths' => ['api/*', 'sanctum/csrf-cookie'],
 
-'allowed_origins_patterns' => [],
+    'allowed_methods' => ['*'],
 
-'allowed_headers' => ['*'],
+    'allowed_origins' => ['https://health-monitoring-peach.vercel.app'],
 
-'exposed_headers' => [],
+    'allowed_origins_patterns' => [],
 
-'max_age' => 0,
+    'allowed_headers' => ['*'],
 
-'supports_credentials' => true, // WAJIB di-set true agar cookie/token bisa lewat
+    'exposed_headers' => [],
+
+    'max_age' => 0,
+
+    'supports_credentials' => true,
+
+];
