7d633bb0028cad6c42a692ca25157f082fdae6d4
efzetx0-wq <efzetmedusa@gmail.com>
2026-06-23 13:41:44 +0800

    fix

--- frontend/src/layouts/MainLayout.jsx
+++ frontend/src/layouts/MainLayout.jsx
@@ -4,11 +4,6 @@ import ReminderChecker from "../components/ReminderChecker";
 import { useEffect, useState } from "react";
 
 import {
-  FaBars,
-  FaTimes
-} from "react-icons/fa";
-
-import {
   getReminders
 } from "../services/reminderService";
 
@@ -18,11 +13,6 @@ export default function MainLayout({
 
   const [reminders, setReminders] =
     useState([]);
-
-  const [
-    mobileSidebarOpen,
-    setMobileSidebarOpen
-  ] = useState(false);
 
   useEffect(() => {
 
@@ -72,135 +62,18 @@ export default function MainLayout({
         reminders={reminders}
       />
 
-      {/* HAMBURGER */}
-      <button
-        onClick={() =>
-          setMobileSidebarOpen(true)
-        }
-        className="
-          md:hidden
-          fixed
-          top-4
-          left-4
+      <div className="flex flex-col md:flex-row">
 
-          z-50
-
-          bg-blue-600
-          text-white
-
-          p-3
-
-          rounded-xl
-
-          shadow-lg
-        "
-      >
-
-        <FaBars />
-
-      </button>
-
-      {/* OVERLAY */}
-      {mobileSidebarOpen && (
-
-        <div
-          onClick={() =>
-            setMobileSidebarOpen(false)
-          }
-          className="
-            md:hidden
-            fixed
-            inset-0
-
-            bg-black/50
-
-            z-40
-          "
-        />
-
-      )}
-
-      {/* MOBILE SIDEBAR */}
-      <div
-        className={`
-          md:hidden
-
-          fixed
-          top-0
-          left-0
-
-          z-50
-
-          transition-transform
-          duration-300
-
-          ${
-            mobileSidebarOpen
-              ? "translate-x-0"
-              : "-translate-x-full"
-          }
-        `}
-      >
-
-        
-          <div
-          className="
-            relative
-
-            w-[60vw]
-
-            min-w-64
-
-            max-w-sm
-          "
-        >
-
-          {/* CLOSE */}
-          <button
-            onClick={() =>
-              setMobileSidebarOpen(false)
-            }
-            className="
-              absolute
-
-              top-4
-              right-4
-
-              z-50
-
-              bg-red-500
-              text-white
-
-              p-2
-
-              rounded-lg
-            "
-          >
-
-            <FaTimes />
-
-          </button>
-
-          <Sidebar />
-
-        </div>
-
-      </div>
-
-      <div className="flex">
-
-        {/* DESKTOP SIDEBAR */}
-        <div className="hidden md:block">
-
-          <Sidebar />
-
-        </div>
+        {/* SIDEBAR COMPONENT (Menangani tampilan Desktop sekaligus Mobile + Auto Hide Scroll) */}
+        <Sidebar />
 
         {/* CONTENT */}
         <main
           className="
             flex-1
             min-h-screen
+            pt-16
+            md:pt-0
           "
         >
 
